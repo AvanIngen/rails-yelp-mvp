c50a5d25076e0561676362a554669a7c8030e16c
Annemarie van Ingen <annvaningen@gmail.com>
2019-11-13 23:24:02 +0100

    not finished yet, the reveiws not working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+restaurants_attributes = [
+  {
+    name:  'Osmans Toechter',
+    address:  'Pappelallee 5, Berlin',
+    phone_number:  '030-12345678',
+    category:  'french'
+  },
+  {
+    name:  'Sushi N8',
+    address:  'Stargarderstr. 6, Berlin',
+    phone_number: '030-23456789',
+    category:  'chinese'
+  },
+    {
+    name:  'Pizzeria La Magica',
+    address:  'Greifenhagener Str. 54, Berlin',
+    phone_number: '030-22808290',
+    category:  'italian'
+  },
+    {
+    name: 'Blumen-Cafe',
+    address: 'Schönhauser Allee 127, Berlin',
+    phone_number: '030-44734226',
+    category:  'japanese'
+  },
+    {
+    name:  'Salt n Bone',
+    address:  'Schliemannstraße 31, Berlin',
+    phone_number: '030-91448885',
+    category:  'belgian'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
